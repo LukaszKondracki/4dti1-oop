aa31459ba5b345d2908fcc551e6007dd1c2a53f1
user <technikum@kondracki.info>
2022-11-03 13:15:12 +0100

    Altorouter

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 require 'vendor/autoload.php';
 
 use Src\Foo;
 
 $f = new Foo();
 $f->bar();
+
+$router = new AltoRouter();
+
+// map homepage
+$router->map( 'GET', '/', function() {
+    echo 'Index';
+});
+
+$router->map('GET', '/foo', function() {
+    echo 'Welcome to foo';
+});
